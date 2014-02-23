3df573fb09d49e04b6eef2e4d5ca67c39239c6eb
perfilyev <perfilyev@nemosoft.ru>
2014-02-23 21:55:31 +0400

    + add hi and low click + generate click pattern + threads

--- src/pro/perfilyev/Metronome/MetronomeActivity.java
+++ src/pro/perfilyev/Metronome/MetronomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
@@ -12,13 +13,28 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MetronomeActivity extends Activity {
   TextView beat;
   TextView bps;
   private ScaleGestureDetector mScaleDetector;
   Integer delayMillis = 500; //1/2 sec = 120bpm; 1 sec = 60bpm
+  double bitRate = 44.1 * 16;
+  private byte[] lowClick;
+  private byte[] hiClick;
+  private byte[] patternClick;
+  private final Mode mode = Mode.PROD;
+
+  private enum Mode {
+    DEV, PROD
+  }
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -35,47 +51,215 @@ public class MetronomeActivity extends Activity {
     win.setAttributes(winParams);
     getActionBar().hide();
 
-    int minBufferSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-            AudioFormat.ENCODING_PCM_16BIT);
-    final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, 44100, AudioTrack.MODE_STREAM);
 
-    int bufferSize = 512;
-    final byte[] buffer = new byte[bufferSize];
+    int minBufferSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_MONO,
+        AudioFormat.ENCODING_PCM_16BIT);
+    final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, 44100, AudioTrack.MODE_STREAM);
 
-    final InputStream inputStream = getResources().openRawResource(R.raw.click);
+    final InputStream lowSource = getResources().openRawResource(R.raw.logic_low);
+    final InputStream hiSource = getResources().openRawResource(R.raw.logic_hi);
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    try {
+      lowSource.skip(44);
+      int reads = lowSource.read();
+      while (reads != -1) {
+        baos.write(reads);
+        reads = lowSource.read();
+      }
+      lowClick = baos.toByteArray();
+
+      baos.reset();
+
+      hiSource.skip(44);
+      reads = hiSource.read();
+      while (reads != -1) {
+        baos.write(reads);
+        reads = hiSource.read();
+      }
+      hiClick = baos.toByteArray();
+
+      lowSource.close();
+      hiSource.close();
+      baos.close();
+    } catch (IOException e) {
+
+    }
+
+
+    patternClick = makePattern();
+
+    double dur = ((patternClick.length) * 8.0) / bitRate;
+
     audioTrack.play();
 
-    final Handler timerHandler = new Handler();
-    Runnable timerRunnable = new Runnable() {
+
+    final Handler widgetHandler = new Handler();
+    final BeatUpdater widgetRunnable = new BeatUpdater() {
       int current = 1;
+      long startTime = System.nanoTime();
 
       @Override
       public void run() {
-        beat.setText(String.valueOf(current));
-        int i = 0;
-        try {
-          while ((i = inputStream.read(buffer)) != -1)
-            audioTrack.write(buffer, 0, i);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
 
-        try {
-          inputStream.reset();
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+        beat.setText(String.valueOf(current));
 
         if (current == 4) {
           current = 1;
-        } else current++;
+        } else {
+          current++;
+        }
 
-        timerHandler.postDelayed(this, delayMillis);
+        widgetHandler.postDelayed(this, delayMillis);
+
+        long processTime = (System.nanoTime() - startTime) / 1000000;
+        long delay = processTime - delayMillis;
+
+        if (mode.equals(Mode.DEV)) {
+          System.out.println("Beat time " + processTime + " ms");
+          System.out.println("Delay time " + delay + " ms");
+        }
+
+        startTime = System.nanoTime();
+      }
+
+      @Override
+      public void sync() {
+        current = 1;
       }
     };
-    timerHandler.postDelayed(timerRunnable, 0);
 
 
+    final BeatTimeUpdater beatTimeUpdater = new BeatTimeUpdater() {
+
+      private long startTime = System.nanoTime();
+
+      @Override
+      public void run() {
+        try {
+
+
+          try {
+            long processTime = (System.nanoTime() - startTime) / 1000000;
+            long delay = processTime - delayMillis;
+            if (mode.equals(Mode.DEV)) {
+
+              System.out.println("Thread sleep time " + processTime + " ms");
+              System.out.println("Delay thread time " + delay + " ms");
+            }
+
+
+            startTime = System.nanoTime();
+            Thread.sleep(delay > 0 ? delayMillis - delay : delayMillis);
+            beat.post(widgetRunnable);
+
+
+            run();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+//       widgetHandler.postDelayed(this, time);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+
+      @Override
+      public void sync() {
+        startTime = System.nanoTime();
+      }
+    };
+    final Thread beatThread = new Thread(beatTimeUpdater);
+
+    Thread clickThread = new Thread(new Runnable() {
+      long startTime = System.nanoTime();
+
+      @Override
+      public void run() {
+        try {
+          long endTime = System.nanoTime();
+          if (mode.equals(Mode.DEV))
+            System.out.println("Click pattern time " + (endTime - startTime) / 0.001 + " s");
+          startTime = System.nanoTime();
+          audioTrack.write(patternClick, 0, patternClick.length);
+//          beatTimeUpdater.sync();
+          widgetRunnable.sync();
+          run();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
+    clickThread.start();
+    widgetHandler.post(widgetRunnable);
+//    beatThread.start();
+//    widgetRunnable.run();
+
+
+  }
+
+  private abstract class BeatThread extends Thread {
+    long startTime = System.nanoTime();
+
+    protected BeatThread(Runnable runnable) {
+      super(runnable);
+    }
+
+    public void sync() {
+      setStartTime(System.nanoTime());
+    }
+
+    public long getStartTime() {
+      return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+      this.startTime = startTime;
+    }
+  }
+
+  private abstract class BeatUpdater implements Runnable {
+    public abstract void sync();
+  }
+
+  private abstract class BeatTimeUpdater implements Runnable {
+    public abstract void sync();
+  }
+
+  private byte[] makePattern() {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    try {
+      baos.write(normalizeClick(hiClick));
+      byte[] normalizedLow = normalizeClick(lowClick);
+      baos.write(normalizedLow);
+      baos.write(normalizedLow);
+      baos.write(normalizedLow);
+
+      baos.write(baos.toByteArray());
+    } catch (IOException e) {
+    }
+    return baos.toByteArray();
+  }
+
+  private double getPatternDuration() {
+    return ((patternClick.length) * 8.0) / bitRate;
+  }
+
+  private byte[] normalizeClick(byte[] click) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    try {
+      baos.write(click);
+      int size = baos.size();
+
+      int needed = (int) (delayMillis * bitRate / 8);
+
+      for (int i = 0; i < needed - size; i++) {
+        baos.write(0);
+      }
+    } catch (IOException e) {
+    }
+    return baos.toByteArray();
   }
 
   @Override
@@ -88,8 +272,13 @@ public class MetronomeActivity extends Activity {
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
       delayMillis = new Float(delayMillis / detector.getScaleFactor()).intValue();
-      bps.setText(String.valueOf(new Double(60 / (delayMillis.doubleValue() / 1000)).intValue()) + "bpm");
+      bps.setText(String.valueOf(new Double(60 / (delayMillis / 1000.0)).intValue()) + "bpm");
       return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+      patternClick = makePattern();
     }
   }
 }
